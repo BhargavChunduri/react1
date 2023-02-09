2f529f07f68cc8e8fdd8b92fc404c020dc9d559b
BhargavChunduri <bhargavchunduri2591@gmail.com>
2023-02-09 19:14:08 +0530

    Updates

--- src/components/careers/ContractJobs.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ContractJobs() {
-  return (
-   <>
-        <h1 className='text-center text-warning'>Welcome To Contract Jobs</h1>
-   </>
-  )
-}
--- src/components/notFound/NotFound.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function NotFound() {
-  return (
-    <div>Error 404: Page Not Found</div>
-  )
-}
